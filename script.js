2b22fca820f60db9b3b47be3e91196657574e5b4
Grindmix <71901283+Grindmix@users.noreply.github.com>
2020-10-11 13:19:53 +0300

    test

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 "use strict"
 
+let end = document.querySelector('#end');
+end.classList.add('closed')
+
+function randomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 function changeButton() {
-  let menu = document.querySelector("#main");
-  let game = document.querySelector("#game");
+  let menu = document.querySelector('#main');
+  let game = document.querySelector('#game');
+
+  menu.style.display = 'none';
 }
