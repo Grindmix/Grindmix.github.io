eb241b75b47693dd0c6ad556e8c899b6c84e562d
Grindmix <71901283+Grindmix@users.noreply.github.com>
2020-10-11 21:09:35 +0300

    Хоба!

--- script.js
+++ script.js
@@ -1,15 +1,142 @@
 "use strict"
-
-let end = document.querySelector('#end');
-end.classList.add('closed');
+let menu = document.querySelector('#mainMenu');
+let game = document.querySelector('#GameDiv');
+let end = document.querySelector('#EndGame');
+let eHead = document.querySelector('#eHead');
+let displayLives;
+let d;
+let input;
+let lives;
+let curword;
+let guess;
+let result;
+let cards;
+let openedCards;
 
 function randomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function changeButton() {
-  let menu = document.querySelector('#main');
-  let game = document.querySelector('#game');
+function startGame() {
 
+  if(typeof cards != "undefined"){
+  cards.forEach(function(item) {
+    item.remove();
+  });
+  }
+
+  lives = 3;
+  cards = [];
+  openedCards = [];
+  let numberOfWord = randomInt(words.length);
+  curword = words[numberOfWord];
+  let cardsContainer = document.querySelector("#cards");
+  let description = document.querySelector('#description');
+  displayLives = document.querySelector('#lives');
+  end.style.display = 'none';
   menu.style.display = 'none';
+  game.style.display = 'inline';
+
+  displayLives.textContent ='Жизни: ' + lives;
+  description.textContent = curword.description;
+
+
+  for (let i = 0; i < curword.name.length; i++){
+  d = document.createElement("div");
+  d.className = "card";
+  cardsContainer.append(d);
+  cards.push(d);
+  }
 }
+
+function check(){
+  input = document.querySelector('#input');
+  guess = input.value.trim().toLowerCase();
+  if (guess.length == 1){
+    checkLetter(guess);
+  }
+  else{
+    checkWord(guess);
+  }
+  input.value = "";
+}
+
+function checkLetter(val) {
+  let found = false;
+  for (let i = 0; i < curword.name.length; i++){
+    if(curword.name[i]==val){
+      openCard(i);
+      found = true;
+    }
+  }
+  if(!found) {
+      takeLive();
+  }
+  if(openedCards.length == cards.length){
+    endGame(true);
+  }
+}
+
+function checkWord(val){
+  if (curword.name == val){
+    endGame(true);
+  }
+  else {
+    endGame(false);
+  }
+}
+
+function takeLive() {
+  lives -= 1;
+  displayLives.textContent ='Жизни: ' + lives;
+  if(lives == 0) {
+    endGame(false);
+  }
+}
+
+function openCard(index){
+  let c = cards[index];
+  if (openedCards.indexOf(c) >= 0) return;
+  c.classList.add("opened");
+  c.textContent = curword.name[index].toUpperCase();
+  openedCards.push(c);
+}
+
+function endGame(result){
+  if(result == false){
+  eHead.textContent = 'Ты проиграл';
+  game.style.display = 'none';
+  end.style.display = 'inline';
+  eHead.style.color = 'red';
+  }
+  else{
+  eHead.textContent = 'Ты выиграл';
+  game.style.display = 'none';
+  end.style.display = 'inline';
+  eHead.style.color = 'green';
+  }
+}
+
+
+var words = [
+{
+  "name": "samsung",
+  "description": "Как называется крупнейшая технологическая компания в Южной Корее?"
+},
+{
+  "name": "алюминий",
+  "description": "Какой металл был открыт Гансом Кристианом Эрстедом в 1825 году?"
+},
+{
+  "name": "хэнкс",
+  "description": "Фамилия актера выигравшего Оскар за фильмы «Филадельфия» (1993) и «Форрест Гамп» (1994)?"
+},
+{
+  "name": "семь",
+  "description": "Сколько игроков в команде по водному поло?"
+  },
+{
+  "name": "queen",
+  "description": "Как называется группа со следующими участниками: Джон Дикон, Брайан Мэй, Фредди Меркьюри, Роджер Тейлор?"
+  }
+]
