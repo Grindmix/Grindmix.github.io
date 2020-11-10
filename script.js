b352be325097b8785f9e15e9ba9fd22afe435b4a
Grindmix <71901283+Grindmix@users.noreply.github.com>
2020-11-10 07:29:51 +0300

    Финальная версия

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 "use strict";
 
 let cell = document.getElementsByClassName('cell');
-let cells = []
 let turnOrder = true;
+let columns;
+let field = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
+];
 
 function turn(row, column) {
   let elem = document.getElementById(`row${row},column${column}`);
-  if (turnOrder == true){
+  if (elem.textContent != ''){
+    return
+  }
+  if (turnOrder == true) {
     elem.textContent = 'X';
+    field[row][column] = 'X';
     turnOrder = false;
   }
-  else{
+  else {
     elem.textContent = 'O';
+    field[row][column] = 'O';
     turnOrder = true;
   }
-  check();
+  winRow();
+  winColumn();
+  winDiagonal();
 }
 
 function newGame() {
-  cells = []
   let input = document.getElementById('cellSize');
   let value = input.value.trim();
-  if(value != ''){
+  if (value != '') {
     for (var i = 0; i < cell.length; i++) {
       cell[i].style.width = `${value}px`;
       cell[i].style.height = `${value}px`;
@@ -38,84 +49,162 @@ function newGame() {
     }
   }
   turnOrder = true;
+  field = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
 }
 
-// Да знаю выглядит ужасно, сделал что мог
+function rowIs(row, val) {
+    return row.every(function(el){
+      return el == val;
+  });
+}
 
-function check() {
+function columnIs(column, val) {
+  return column.every(function(el){
+    return el == val;
+  });
+}
+
+function makeCol() {
+  columns = [
+    [],
+    [],
+    []
+  ];
+  for (var i = 0; i < field.length; i++) {
+    columns[0].push(field[i][0]);
+    columns[1].push(field[i][1]);
+    columns[2].push(field[i][2]);
+  }
+}
+
+function winRow() {
+  for (var i = 0; i < field.length; i++) {
+    if (rowIs(field[i], 'X')) {
+      paint(`row${i}`);
+    }
+    else if (rowIs(field[i], 'O')) {
+      paint(`row${i}`);
+    }
+  }
+}
+
+function winColumn() {
+  makeCol();
+  for (var i = 0; i < columns.length; i++) {
+    if (columnIs(columns[i], 'X')){
+      paint(`col${i}`);
+    }
+    else if (columnIs(columns[i], 'O')) {
+      paint(`row${i}`);
+    }
+  }
+}
+
+function winDiagonal() {
+  let j = 0;
+  let XCounterLeft = 0;
+  let OcounterLeft = 0;
+  let XCounterRight = 0;
+  let OcounterRight = 0;
+  for (var i = 0; i < field.length; i++) {
+    if (field[i][i] == 'X'){
+      XCounterLeft++;
+    }
+    else if (field[i][i] == 'O') {
+      OcounterLeft++;
+    }
+  }
+  for (var i = 2; i > -1; i--) {
+    if (field[j][i] == 'X'){
+      XCounterRight++;
+      j++;
+    }
+    else if (field[j][i] == 'O') {
+      OcounterRight++;
+      j++;
+    }
+  }
+  if (XCounterLeft == 3 || OcounterLeft == 3) {
+    paint('diagLeft');
+  }
+  else if(XCounterRight == 3 || OcounterRight == 3){
+    paint('diagRight');
+  }
+}
+
+function paint(typeOfWin) {
+  let cells = [];
   for (var i = 0; i < cell.length; i++){
     cells.splice(i, 1 ,cell[i].textContent);
   }
-  if (cells[0] != '' && cells[1] != '' && cells[2] != '' // row 1
-  && cells[0] == cells[1] && cells[1] == cells[2]) {
+switch (typeOfWin) {
+  case 'row0':
     cell[0].style.background = 'yellow';
     cell[1].style.background = 'yellow';
     cell[2].style.background = 'yellow';
     cell[0].style.color = 'black';
     cell[1].style.color = 'black';
     cell[2].style.color = 'black';
-  }
-  else if (cells[3] != '' && cells[4] != '' && cells[5] != '' // row 2
-  && cells[3] == cells[4] && cells[4] == cells[5]) {
+    break;
+  case 'row1':
     cell[3].style.background = 'yellow';
     cell[4].style.background = 'yellow';
     cell[5].style.background = 'yellow';
     cell[3].style.color = 'black';
     cell[4].style.color = 'black';
     cell[5].style.color = 'black';
-  }
-  else if (cells[6] != '' && cells[7] != '' && cells[8] != '' // row 3
-  && cells[6] == cells[7] && cells[7] == cells[8]) {
+    break;
+  case 'row2':
     cell[6].style.background = 'yellow';
     cell[7].style.background = 'yellow';
     cell[8].style.background = 'yellow';
     cell[6].style.color = 'black';
     cell[7].style.color = 'black';
     cell[8].style.color = 'black';
-  }
-  else if (cells[0] != '' && cells[3] != '' && cells[6] != '' // column 1
-  && cells[0] == cells[3] && cells[3] == cells[6]) {
+    break;
+  case 'col0':
     cell[0].style.background = 'yellow';
     cell[3].style.background = 'yellow';
     cell[6].style.background = 'yellow';
     cell[0].style.color = 'black';
     cell[3].style.color = 'black';
     cell[6].style.color = 'black';
-  }
-  else if (cells[1] != '' && cells[4] != '' && cells[7] != '' // column 2
-  && cells[1] == cells[4] && cells[4] == cells[7]) {
+    break;
+  case 'col1':
     cell[1].style.background = 'yellow';
     cell[4].style.background = 'yellow';
     cell[7].style.background = 'yellow';
     cell[1].style.color = 'black';
     cell[4].style.color = 'black';
     cell[7].style.color = 'black';
-  }
-  else if (cells[2] != '' && cells[5] != '' && cells[8] != '' // column 3
-  && cells[2] == cells[5] && cells[5] == cells[8]) {
+    break;
+  case 'col2':
     cell[2].style.background = 'yellow';
     cell[5].style.background = 'yellow';
     cell[8].style.background = 'yellow';
     cell[2].style.color = 'black';
     cell[5].style.color = 'black';
     cell[8].style.color = 'black';
-  }
-  else if (cells[2] != '' && cells[4] != '' && cells[6] != '' // diagonal left to right
-  && cells[2] == cells[4] && cells[4] == cells[6]) {
+    break;
+  case 'diagRight':
     cell[2].style.background = 'yellow';
     cell[4].style.background = 'yellow';
     cell[6].style.background = 'yellow';
     cell[2].style.color = 'black';
     cell[4].style.color = 'black';
     cell[6].style.color = 'black';
-  }
-  else if (cells[0] != '' && cells[4] != '' && cells[8] != '' // diagonal right to left
-  && cells[0] == cells[4] && cells[4] == cells[8]) {
+    break;
+  case 'diagLeft':
     cell[0].style.background = 'yellow';
     cell[4].style.background = 'yellow';
     cell[8].style.background = 'yellow';
     cell[0].style.color = 'black';
     cell[4].style.color = 'black';
     cell[8].style.color = 'black';
+    break;
   }
 }
